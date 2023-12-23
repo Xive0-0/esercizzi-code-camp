02862b2e43d0023c13f63460d1ac983275d96050
Xive0-0 <142550222+Xive0-0@users.noreply.github.com>
2023-12-23 21:59:35 +0100

    Create app.ts

---
+++ app.ts
@@ -0,0 +1,42 @@
+function translatePigLatin(str: string): string | string[] {
+  str.toLocaleLowerCase();
+  let token: number = 0;
+  let contenitore: string[] = str.split("");
+  let somma: string = "";
+  for (let i = 0; i < contenitore.length; i++) {
+    if (
+      contenitore[i] == "b" ||
+      "c" ||
+      "d" ||
+      "f" ||
+      "G" ||
+      "h" ||
+      "l" ||
+      "m" ||
+      "n" ||
+      "p" ||
+      "q" ||
+      "r" ||
+      "s" ||
+      "t" ||
+      "v" ||
+      "z"
+    ) {
+      contenitore.push(contenitore[i]);
+      contenitore.splice(i, 1);
+
+      token++;
+    }
+    if (token == 0) {
+      for (let i = 0; i < contenitore.length; i++) {
+        somma = somma + contenitore[i];
+      }
+      return somma + "way";
+    }
+  }
+  for (let i = 0; i < contenitore.length; i++) {
+    somma = somma + contenitore[i];
+  }
+  return somma + "ay";
+}
+console.log(translatePigLatin("consonant"))
